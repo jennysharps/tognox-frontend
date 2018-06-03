d6817d6fe629bb1eca0ad24d2df8f6e4011230d8
Jenny Sharps <jlsharps@deloitte.co.uk>
2018-06-03 08:37:54 +0100

    Add enzyme test setup

--- src/shared/App.test.js
+++ src/shared/App.test.js
@@ -1,8 +1,10 @@
+import { shallow } from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  it('should render without crashing', () => {
+    const initialStore = {};
+    shallow(<App store={mockStore(initialStore)} />);
+  });
 });
